1a84348fb8157b2410f39260d06fa90cc4676d57
p1x3lmx <33325715+p1x3lmx@users.noreply.github.com>
2022-08-24 07:04:34 -0500

    Add edit brand page.

---
+++ Beercyclopedia/Pages/Brands/Edit.cshtml.cs
@@ -0,0 +1,35 @@
+using Beercyclopedia.Data;
+using Beercyclopedia.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Beercyclopedia.Pages.Brands;
+
+[BindProperties]
+public class EditModel : PageModel
+{
+    private readonly ApplicationDbContext _db;
+    public Brand Brand { get; set; }
+
+    public EditModel(ApplicationDbContext db)
+    {
+        _db = db;
+    }
+
+    public void OnGet(int id)
+    {
+        Brand = _db.Brands.Find(id);
+    }
+
+    public async Task<IActionResult> OnPost()
+    {
+        if (ModelState.IsValid)
+        {
+            _db.Brands.Update(Brand);
+            await _db.SaveChangesAsync();
+            return RedirectToPage("Index");
+        }
+
+        return RedirectToPage("Index");
+    }
+}
