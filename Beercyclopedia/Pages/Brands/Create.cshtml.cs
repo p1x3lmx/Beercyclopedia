385d9242232272b9b3daae20862433be43c5a40e
p1x3lmx <33325715+p1x3lmx@users.noreply.github.com>
2022-08-23 21:38:01 -0500

    Added the create brand page, and solve a typo in the Create Style html.

---
+++ Beercyclopedia/Pages/Brands/Create.cshtml.cs
@@ -0,0 +1,29 @@
+using Beercyclopedia.Data;
+using Beercyclopedia.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Beercyclopedia.Pages.Brands;
+
+public class CreateModel : PageModel
+{
+    private readonly ApplicationDbContext _db;
+    public Brand Brand { get; set; }
+
+    public CreateModel(ApplicationDbContext db)
+    {
+        _db = db;
+    }
+    
+    public void OnGet()
+    {
+        
+    }
+    
+    public async Task<IActionResult> OnPost(Brand brand)
+    {
+        await _db.Brands.AddAsync(brand);
+        await _db.SaveChangesAsync();
+        return RedirectToPage("Index");
+    }
+}
--- Beercyclopedia/Pages/Brands/Index.cshtml.cs
+++ Beercyclopedia/Pages/Brands/Index.cshtml.cs
@@ -1,11 +1,20 @@
+using Beercyclopedia.Data;
+using Beercyclopedia.Model;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Beercyclopedia.Pages.Brands;
 
 public class IndexModel : PageModel
 {
+    private readonly ApplicationDbContext _db;
+    public IEnumerable<Brand> Brands;
+
+    public IndexModel(ApplicationDbContext db)
+    {
+        _db = db;
+    }
     public void OnGet()
     {
-        
+        Brands = _db.Brands;
     }
 }
