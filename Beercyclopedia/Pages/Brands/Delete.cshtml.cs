03bffd552a6eed68679439078350eed776fcf907
p1x3lmx <33325715+p1x3lmx@users.noreply.github.com>
2022-09-01 07:19:16 -0500

    Added the logic to display the delete page in brands, the delete post is not implemented yet.

---
+++ Beercyclopedia/Pages/Brands/Delete.cshtml.cs
@@ -0,0 +1,32 @@
+using Beercyclopedia.Data;
+using Beercyclopedia.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Beercyclopedia.Pages.Brands;
+
+public class DeleteModel : PageModel
+{
+    public readonly ApplicationDbContext _db;
+    [BindProperty] 
+    public Brand Brand { get; set; }
+
+    public bool IsInUse { get; set; }
+
+    public DeleteModel(ApplicationDbContext db)
+    {
+        _db = db;
+    }
+
+    public void OnGet(int id)
+    {
+        var firstFound = _db.Beers.FirstOrDefault((s => s.Brands.Id == id));
+        if (firstFound != null)
+        {
+            IsInUse = true;
+            Brand = _db.Brands.Find(id);
+        }
+        Brand = _db.Brands.Find(id);
+    }
+        
+}
