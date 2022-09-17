b4a0f85db2846a5f9c20d2cff5d11a64429f73e7
p1x3lmx <33325715+p1x3lmx@users.noreply.github.com>
2022-09-17 09:37:38 -0500

    Added Detail view page and number rating changed to star rating according with the number.

---
+++ Beercyclopedia/Pages/Beers/Detail.cshtml.cs
@@ -0,0 +1,29 @@
+using Beercyclopedia.Data;
+using Beercyclopedia.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Beercyclopedia.Pages.Beers;
+
+public class DetailModel : PageModel
+{
+    
+    private readonly ApplicationDbContext _db;
+    public Beer Beer { get; set; }
+    public Style Style { get; set; }
+    public Brand Brand { get; set; }
+
+    public DetailModel(ApplicationDbContext db)
+    {
+        _db = db;
+    }
+
+    public void OnGet(int id)
+    {
+        Beer = _db.Beers.Find(id);
+        Style = _db.Styles.Find(Beer.StyleId);
+        Brand = _db.Brands.Find(Beer.BrandId);
+    } 
+    
+}
