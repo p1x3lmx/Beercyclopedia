e31341d01af1f710d9e5394b5738b008492ce59d
p1x3lmx <33325715+p1x3lmx@users.noreply.github.com>
2022-09-18 15:42:40 -0500

    Added duplicate beer logic to index beer.

--- Beercyclopedia/Pages/Beers/Index.cshtml.cs
+++ Beercyclopedia/Pages/Beers/Index.cshtml.cs
@@ -1,5 +1,6 @@
 using Beercyclopedia.Data;
 using Beercyclopedia.Model;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 
@@ -10,15 +11,33 @@ public class IndexModel : PageModel
     private readonly ApplicationDbContext _db;
     public IEnumerable<Beer> Beers { get; set; }
     public IEnumerable<Brand> Brands { get; set; }
+    public Beer Beer { get; set; }
+    public string Message { get; set; }
 
     public IndexModel(ApplicationDbContext db)
     {
         _db = db;
     }
+    
     public void OnGet()
     {
         Beers = _db.Beers.Include(b => b.Brands).Include(b=>b.Styles);
+    }
 
+    public async Task<IActionResult> OnPostDuplicate(int id)
+    {
+        Beer = _db.Beers.Find(id);
+        
+        Beer BeerDuplicate = new Beer()
+        {
+            Name = Beer.Name,
+            BrandId = Beer.BrandId,
+            StyleId = Beer.StyleId,
+            Rating = Beer.Rating
+        };
+        await _db.Beers.AddAsync(BeerDuplicate);
+        await _db.SaveChangesAsync();
+        return RedirectToPage("Index");
     }
     
 }
