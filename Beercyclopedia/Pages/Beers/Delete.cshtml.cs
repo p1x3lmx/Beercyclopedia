a15c4693eb8bc2276236a72f36b6995267ca7b95
p1x3lmx <33325715+p1x3lmx@users.noreply.github.com>
2022-09-19 20:38:25 -0500

    Added delete beer logic.

---
+++ Beercyclopedia/Pages/Beers/Delete.cshtml.cs
@@ -0,0 +1,31 @@
+using Beercyclopedia.Data;
+using Beercyclopedia.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Beercyclopedia.Pages.Beers;
+
+public class DeleteModel : PageModel
+{
+    private readonly ApplicationDbContext _db;
+    public Beer Beer { get; set; }
+
+    public DeleteModel(ApplicationDbContext db)
+    {
+        _db = db;
+    }
+
+    public void OnGet(int id)
+    {
+        Beer = _db.Beers.Find(id);
+    }
+    
+    public async Task<IActionResult> OnPost(Beer beer)
+    {
+        _db.Beers.Remove(beer);
+        await _db.SaveChangesAsync();
+        return RedirectToPage("Index");
+    }
+    
+}
