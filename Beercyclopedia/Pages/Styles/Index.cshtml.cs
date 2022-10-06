eab23cf1378a1a35554382aa7a0225eda54e2770
p1x3lmx <33325715+p1x3lmx@users.noreply.github.com>
2022-10-06 06:05:54 -0500

    Added duplicate style button.

--- Beercyclopedia/Pages/Styles/Index.cshtml.cs
+++ Beercyclopedia/Pages/Styles/Index.cshtml.cs
@@ -1,5 +1,6 @@
 using Beercyclopedia.Data;
 using Beercyclopedia.Model;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 
@@ -10,6 +11,8 @@ public class IndexModel : PageModel
     private readonly ApplicationDbContext _db;
     public IEnumerable<Style> Styles;
 
+    public Style Style { get; set; }
+
     public IndexModel(ApplicationDbContext db)
     {
         _db = db;
@@ -18,5 +21,18 @@ public class IndexModel : PageModel
     public void OnGet()
     {
         Styles = _db.Styles;
+    }
+    
+    public async Task<IActionResult> OnPostDuplicate(int id)
+    {
+        Style = _db.Styles.Find(id);
+        
+        Style StyleDuplicate = new Style()
+        {
+            Name = $"{Style.Name} Duplicate",
+        };
+        await _db.Styles.AddAsync(StyleDuplicate);
+        await _db.SaveChangesAsync();
+        return RedirectToPage("Index");
     }
 }
