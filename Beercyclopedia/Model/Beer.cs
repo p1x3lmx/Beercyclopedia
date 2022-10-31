fd20d3d982748973170aab9afa2646b421348c9a
p1x3lmx <33325715+p1x3lmx@users.noreply.github.com>
2022-10-30 22:20:16 -0600

    Added prompt data annotations for create beer.

--- Beercyclopedia/Model/Beer.cs
+++ Beercyclopedia/Model/Beer.cs
@@ -8,20 +8,23 @@ public class Beer
     [Key]
     public int Id { get; set; }
     [Required(ErrorMessage = "Beer name is required.")]
-    [Display(Name = "Beer Name")]
+    [Display(Name = "Beer Name", Prompt = "Enter Beer Name")]
     public string Name { get; set; }
     [Required(ErrorMessage = "Please select a brand")]
-    [Display(Name = "Brand")]
+    [Display(Name = "Beer Brand",Prompt = "Enter Beer Brand")]
     public int BrandId { get; set; }
     [Required(ErrorMessage = "Please select a Style")]
-    [Display(Name = "Style")]
+    [Display(Name = "Style",Prompt = "BCJP Style")]
     public int StyleId { get; set; }
     [Range(1, 5, ErrorMessage = "Rating Value for {0} must be {1} and {2}")]
+    [Display(Name = "Beer Rating", Prompt = "Enter Beer Rating")]
     public int Rating { get; set; }
     [Range(0, 200, ErrorMessage = "IBU Value should be between {1} and {2}")]
+    [Display(Name = "IBU", Prompt = "Beer IBU")]
     public int IBU { get; set; }
     public string Description { get; set; }
     [ValidateNever]
+    [Display(Prompt = "Select Beer Brand")]
     public Brand Brands { get; set; }
     [ValidateNever]
     public Style Styles { get; set; }
