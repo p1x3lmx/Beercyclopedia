88df27cb80b395129b86cff770d607b99cf17510
p1x3lmx <33325715+p1x3lmx@users.noreply.github.com>
2022-11-01 07:04:44 -0600

    Added Sorting link to branding.

--- Beercyclopedia/Pages/Brands/Index.cshtml.cs
+++ Beercyclopedia/Pages/Brands/Index.cshtml.cs
@@ -2,6 +2,8 @@ using Beercyclopedia.Data;
 using Beercyclopedia.Model;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
 
 namespace Beercyclopedia.Pages.Brands;
 
@@ -10,25 +12,37 @@ public class IndexModel : PageModel
     private readonly ApplicationDbContext _db;
     public IEnumerable<Brand> Brands;
     public Brand Brand { get; set; }
+    public String BrandSort { get; set; }
 
     public IndexModel(ApplicationDbContext db)
     {
         _db = db;
     }
-    public void OnGet()
+
+    public async Task OnGetAsync(string sortOrder)
     {
-        Brands = _db.Brands;
+        BrandSort = String.IsNullOrEmpty(sortOrder) ? "brand_desc" : "";
+        IQueryable<Brand> brandIQ = from s in _db.Brands select s;
+
+        switch (sortOrder)
+        {
+            case "brand_desc":
+                brandIQ = brandIQ.OrderByDescending(s => s.Name);
+                break;
+            default:
+                brandIQ = brandIQ.OrderBy(s => s.Name);
+                break;
+        }
+
+        Brands = await brandIQ.AsNoTracking().ToListAsync();
     }
-    
+
     public async Task<IActionResult> OnPostDuplicate(int id)
     {
         Brand = _db.Brands.Find(id);
-        
-        Brand BrandDuplicate = new Brand()
-        {
-            Name = $"{Brand.Name} Duplicate",
-        };
-        
+
+        Brand BrandDuplicate = new Brand() {Name = $"{Brand.Name} Duplicate",};
+
         await _db.Brands.AddAsync(BrandDuplicate);
         await _db.SaveChangesAsync();
         TempData["success"] = "Brand duplicated successfully";
