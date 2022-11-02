5e14e1d2b2b470df99769f5dfeaa5f7929fd27d6
p1x3lmx <33325715+p1x3lmx@users.noreply.github.com>
2022-11-02 06:46:55 -0600

    Added prompt to input beer brand in create brand page. Layout added class fixed-bottom to footer.

--- Beercyclopedia/Model/Brand.cs
+++ Beercyclopedia/Model/Brand.cs
@@ -6,6 +6,7 @@ public class Brand
 {
     public int Id { get; set; }
     [Required(ErrorMessage = "Please enter a valid beer brand name.")]
+    [Display(Name = "Brewery Brand", Prompt = "Beer Brand")]
     public string Name { get; set; }
     
 }
